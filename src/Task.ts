79438e3b382d4fee4f63e7ecda6674c5ec27e19c
max-storyhealth <78374703+max-storyhealth@users.noreply.github.com>
2024-07-30 10:46:43 -0700

    Update comment formatting for RequestPriority enum

--- src/Task.ts
+++ src/Task.ts
@@ -38,11 +38,13 @@ export enum TaskIntentKind {
   _instanceOrder = "instance-order",
   _option = "option",
 }
-// N.B. This is what we've been using for setting the priority field on a Task in the core repo. 
-// By the FHIR spec, priority is actually a Code field, https://hl7.org/fhir/R4/task-definitions.html#Task.priority, 
-// where the appropriate code options should be from this valueset: from https://hl7.org/fhir/R4/valueset-request-priority.html
-// We have been using the upper case subset defined below throughout our code, which doesn't match the spec. 
-// To ensure we use only expected values for the field, we are updating the priority field type here to use this enum.
+/**
+ * N.B. This is what we've been using for setting the priority field on a Task in the core repo. 
+ * By the FHIR spec, priority is actually a Code field, https://hl7.org/fhir/R4/task-definitions.html#Task.priority, 
+ * where the appropriate code options should be from this valueset: from https://hl7.org/fhir/R4/valueset-request-priority.html
+ * We have been using the upper case subset defined below throughout our code, which doesn't match the spec. 
+ * To ensure we use only expected values for the field, we are updating the priority field type here to use this enum.
+ */
 export enum RequestPriority {
     STAT = 'STAT',
     URGENT = 'URGENT',
